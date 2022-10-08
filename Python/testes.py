8fe3845dbc9ee69636082951ca9d8fffe864867d
Andréa J S França <dejandreafranca@gmail.com>
2022-10-08 09:07:19 -0300

    first

---
+++ Python/testes.py
@@ -0,0 +1,15 @@
+#variáveis e constantes
+
+
+#variáveis
+age = 32
+name = "Andrea"
+
+print(f'Meu nome é {name} e eu tenho {age} anos')
+
+age, name = 23,"Aria"
+print(f'Olá {name} você tem {age} anos')
+
+
+#constantes - Python não tem constantes
+
